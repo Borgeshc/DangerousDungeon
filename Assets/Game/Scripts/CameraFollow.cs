510703d61d502a3b8295a3692e37119ae72168bf
Borgeshc <Borgeshc@gmatc.matc.edu>
2016-12-19 22:45:42 -0600

    Updated animations, camera movement, and camera effects

--- Assets/Game/Scripts/CameraFollow.cs
+++ Assets/Game/Scripts/CameraFollow.cs
@@ -17,15 +17,6 @@ public class CameraFollow : MonoBehaviour {
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(transform.position.x <= 0)
-        {
-            cameraOffset.x = 5;
-        }
-        if(transform.position.x >= 35)
-        {
-            cameraOffset.x = -5;
-        }
         transform.position = Vector3.Lerp(transform.position, new Vector3(player.transform.position.x + cameraOffset.x, player.transform.position.y + cameraOffset.y, player.transform.position.z + cameraOffset.z), Time.deltaTime * cameraSpeed);
-
     }
 }
