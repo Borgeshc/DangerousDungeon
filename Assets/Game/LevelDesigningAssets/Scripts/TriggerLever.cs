cbbc20a706be88efd46c2edb76a8f7179d7c0505
Borgeshc <Borgeshc@gmatc.matc.edu>
2016-12-20 22:55:26 -0600

    Added level 2, added traps, and levers

---
+++ Assets/Game/LevelDesigningAssets/Scripts/TriggerLever.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerLever : MonoBehaviour
+{
+    public Animator openDoorAnimator;
+    public GameObject interactionMessage;
+    Animator anim;
+    bool wasPressed;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if(!wasPressed)
+            interactionMessage.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E) || Input.GetButtonDown("Fire1"))
+            {
+                wasPressed = true;
+                interactionMessage.SetActive(false);
+                anim.SetBool("IsPulled", true);
+                openDoorAnimator.SetBool("WasOpened", true);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            interactionMessage.SetActive(false);
+        }
+    }
+}
